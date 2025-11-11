99f475fbd5f6e723291d5af135836deaf577c149
Divyadharshini.V <divyadharshinivenkat31@gmail.com>
2025-11-11 14:56:03 +0530

    Add model evaluation results for Logistic Regression and Decision Tree
    Output results for Logistic Regression and Decision Tree models, including accuracy, confusion matrices, and classification metrics.

---
+++ outputmini4.py
@@ -0,0 +1,34 @@
+Logistic Regression Accuracy: 0.80
+[[90 15]
+ [21 53]]
+              precision    recall  f1-score   support
+
+           0       0.81      0.86      0.83       105
+           1       0.78      0.72      0.75        74
+
+    accuracy                           0.80       179
+   macro avg       0.80      0.79      0.79       179
+weighted avg       0.80      0.80      0.80       179
+
+
+Decision Tree Accuracy: 0.72
+[[79 26]
+ [24 50]]
+              precision    recall  f1-score   support
+
+           0       0.77      0.75      0.76       105
+           1       0.66      0.68      0.67        74
+
+    accuracy                           0.72       179
+   macro avg       0.71      0.71      0.71       179
+weighted avg       0.72      0.72      0.72       179
+
+/usr/local/lib/python3.12/dist-packages/sklearn/linear_model/_logistic.py:465: ConvergenceWarning: lbfgs failed to converge (status=1):
+STOP: TOTAL NO. OF ITERATIONS REACHED LIMIT.
+
+Increase the number of iterations (max_iter) or scale the data as shown in:
+    https://scikit-learn.org/stable/modules/preprocessing.html
+Please also refer to the documentation for alternative solver options:
+    https://scikit-learn.org/stable/modules/linear_model.html#logistic-regression
+  n_iter_i = _check_optimize_result(
+Colab paid products - Cancel contracts here
