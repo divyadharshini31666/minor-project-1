4952f8053f0dc0931176eecd328fed4513de18ef
Divyadharshini.V <divyadharshinivenkat31@gmail.com>
2025-11-11 14:20:27 +0530

    Create outputmin1.py

---
+++ outputmin1.py
@@ -0,0 +1,37 @@
+Logistic Regression Accuracy: 0.75
+[[79 20]
+ [18 37]]
+              precision    recall  f1-score   support
+
+           0       0.81      0.80      0.81        99
+           1       0.65      0.67      0.66        55
+
+    accuracy                           0.75       154
+   macro avg       0.73      0.74      0.73       154
+weighted avg       0.76      0.75      0.75       154
+
+
+Decision Tree Accuracy: 0.75
+[[75 24]
+ [15 40]]
+              precision    recall  f1-score   support
+
+           0       0.83      0.76      0.79        99
+           1       0.62      0.73      0.67        55
+
+    accuracy                           0.75       154
+   macro avg       0.73      0.74      0.73       154
+weighted avg       0.76      0.75      0.75       154
+
+
+Random Forest Accuracy: 0.72
+[[77 22]
+ [21 34]]
+              precision    recall  f1-score   support
+
+           0       0.79      0.78      0.78        99
+           1       0.61      0.62      0.61        55
+
+    accuracy                           0.72       154
+   macro avg       0.70      0.70      0.70       154
+weighted avg       0.72      0.72      0.72       154
